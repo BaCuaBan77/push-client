c5cd5ae494b67f230a54ee18555b86ec3d37dad5
Long Le <long.le@observis.fi>
2019-10-14 00:02:57 +0300

    init

---
+++ src/scripts/utils/http.js
@@ -0,0 +1,40 @@
+const host = process.env.PUSH_SERVER_URL;
+
+function post(path, body) {
+  return fetch(`http://localhost:5000/subscription`, {
+    credentials: "omit",
+    headers: { "content-type": "application/json;charset=UTF-8", "sec-fetch-mode": "cors" },
+    body: JSON.stringify(body),
+    method: "POST",
+    mode: "cors"
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      return data;
+    });
+}
+
+function get(path) {
+  console.log("get to Get");
+  return fetch(`http://localhost:5000${path}`, {
+    credentials: "omit",
+    headers: { "content-type": "application/json;charset=UTF-8", "sec-fetch-mode": "cors" },
+    method: "GET",
+    mode: "cors"
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      return data;
+    });
+}
+
+const http = {
+  post: post,
+  get: get
+};
+
+export default http;
